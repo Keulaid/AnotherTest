08996ece69c10d100387ca21c933e79d6441b79b
Ashton Cross <ashtoncrossdude@gmail.com>
2018-02-14 10:56:28 -0700

    Random Number Generator

--- JavaProgram/SchoolProject/src/school_Project/new_test.java
+++ JavaProgram/SchoolProject/src/school_Project/new_test.java
@@ -1,6 +1,6 @@
 //Hello from mac!
 package school_Project;
-
+import java.util.*;
 /**
  * This was written for class.
  * @author ashtoncrossdude
@@ -9,9 +9,20 @@ public class new_test { // Remember to always make the main class public.
     //ARGS
     public static void main(String[] arguments) {
         // TODO learn stuff & things. (remember to write page number in text book)
-        System.out.println("My favorite " + arguments[1] + " is " + arguments[2] + "."); 
-       
+        /* Because we imported the java.util.* classes, we dont need to say...
+            java.util.Random
+        We just say
+        Random
+        */
         
+        Random generator = new Random();
+        int Random_Value = generator.nextInt();
+        System.out.println(Random_Value);
+        //or we could probably just do...
+        System.out.println(generator.nextInt());
+        
+        //java.util.Random genorator = new java.util.Random();
+        // Check donwload.java.net/jdk8/docs/api for all the classes.
         // Read up on page 49 - 51 on how the arguments work in the configuration file.
         
     } 
