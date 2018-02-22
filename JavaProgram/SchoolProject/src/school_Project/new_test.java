eb94468fe7c1aaaae0493849a5a3ee52e2d20f8e
Ashton Cross <ashtoncrossdude@gmail.com>
2018-02-22 10:55:03 -0700

    The types of strings.

--- JavaProgram/SchoolProject/src/school_Project/new_test.java
+++ JavaProgram/SchoolProject/src/school_Project/new_test.java
@@ -1,6 +1,5 @@
 //Hello from mac!
 package school_Project;
-import java.util.*;
 /**
  * This was written for class.
  * @author ashtoncrossdude
@@ -8,22 +7,14 @@ import java.util.*;
 public class new_test { // Remember to always make the main class public.
     //ARGS
     public static void main(String[] arguments) {
-        // TODO learn stuff & things. (remember to write page number in text book)
-        /* Because we imported the java.util.* classes, we dont need to say...
-            java.util.Random
-        We just say
-        Random
-        */
+        char Char_Variable = 'c'; // chars use the ' quotes, and only have one letter.
+        String String_Variable = "Wow"; // strings use as many characters as the computer can provide
+        int Int_Variable = 88; // integers hold numbers (integer numbers in specific)
+        double Double_Variable = 3.1; // basically just integers with decimals.
+        float Float_Variable_Farenheight = 3.1F; // this specifies what type of number it is. This is so that you can't add 3.1F with 3.1C, or 30M with 30MM.
+        System.out.println(Char_Variable);
+        System.out.println(String_Variable);
         
-        Random generator = new Random();
-        int Random_Value = generator.nextInt();
-        System.out.println(Random_Value);
-        //or we could probably just do...
-        System.out.println(generator.nextInt());
-        
-        //java.util.Random genorator = new java.util.Random();
-        // Check donwload.java.net/jdk8/docs/api for all the classes.
-        // Read up on page 49 - 51 on how the arguments work in the configuration file.
         
     } 
 }
