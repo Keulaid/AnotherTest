668f1af9bda747d16c63c556be2089232f6d65c1
Ashton Cross <ashtoncrossdude@gmail.com>
2018-02-28 10:44:46 -0700

    print(); VS println();

--- JavaProgram/SchoolProject/src/school_Project/new_test.java
+++ JavaProgram/SchoolProject/src/school_Project/new_test.java
@@ -7,17 +7,24 @@ package school_Project;
 public class new_test { // Remember to always make the main class public.
     //ARGS
     public static void main(String[] arguments) {
-        char Char_Variable = 'c'; // chars use the ' quotes, and only have one letter.
-        String String_Variable = "Wow"; // strings use as many characters as the computer can provide
-        int Int_Variable = 88; // integers hold numbers (integer numbers in specific)
-        double Double_Variable = 3.1; // basically just integers with decimals.
-        float Float_Variable_Farenheight = 3.1F; // this specifies what type of number it is. This is so that you can't add 3.1F with 3.1C, or 30M with 30MM.
-        System.out.println(Char_Variable);
-        System.out.println(String_Variable);
-        System.out.println(Float_Variable_Farenheight);
-        System.out.println(Double_Variable);
+        // print VS println
+        /*
+        Print just allows you to print a message, and let it mix with the other
+        message. Println makes it its own line to prevent that.
         
+        */
         
+        System.out.println("Wow, my line is on it's own!");
+        System.out.print('w'); 
+        System.out.print('o');
+        System.out.print('w');
+        System.out.println();
+        
+        /*
+            Also, when you use println(); you can leave it empty just so you
+        leave the line clear. It's kinda fancy. This doesn't work with print();
+        however.
+        */
         
         
     } 
